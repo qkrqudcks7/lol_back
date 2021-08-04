9942e770f7773a902b500e22bcdd6211eea4ade7
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-05 01:06:10 +0900

    swagger 추가

---
+++ src/main/java/lol/demo/swagger/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package lol.demo.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version ="V1";
+        title = "Park.GG API" + version;
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.parkgg.swaggerexam.api.v1"))
+                .paths(PathSelectors.ant("/v1/api/**"))
+                .build()
+                .apiInfo(apiInfo(title, version));
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "Swagger로 생성한 API Docs",
+                version,
+                "www.example.com",
+                new Contact("Contact Me", "www.example.com", "pak7380@naver.com"),
+                "Licenses",
+                "pak7380@naver.com",
+                new ArrayList<>()
+        );
+    }
+
+}
